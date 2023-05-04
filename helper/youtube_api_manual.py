2cd3b2de6f80544975a63bd4df604f005add444a
viktorlov <orlovva@ymail.com>
2023-05-04 15:32:41 +0300

    (l) code review requirements.txt && helper

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -1,11 +1,9 @@
 import json
 import os
 
+import isodate
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
-
-import isodate
-
 
 # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
 api_key: str = os.getenv('YT_API_KEY')
@@ -30,7 +28,6 @@ channel_id = 'UC1eFXmJNkjITxPFWTy6RsWg'  # Редакция
 channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
 printj(channel)
 
-
 '''
 получить данные по play-листам канала
 docs: https://developers.google.com/youtube/v3/docs/playlists/list
@@ -43,7 +40,6 @@ playlists = youtube.playlists().list(channelId=channel_id,
 for playlist in playlists['items']:
     print(playlist)
     print()
-
 
 '''
 получить данные по видеороликам в плейлисте
@@ -80,7 +76,6 @@ for video in video_response['items']:
     iso_8601_duration = video['contentDetails']['duration']
     duration = isodate.parse_duration(iso_8601_duration)
     print(duration)
-
 
 '''
 получить статистику видео по его id
