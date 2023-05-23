ef43bd4f39af39196c3d4213d1590ed086998e73
viktorlov <orlovva@ymail.com>
2023-05-23 12:36:17 +0300

    (l) totally code review <<homework-5>>

--- src/channel.py
+++ src/channel.py
@@ -9,92 +9,130 @@ from env_sett import ENV_FILE
 load_dotenv(ENV_FILE)
 
 
-class Channel:
-    """
-    Класс для ютуб-канала.
-    """
-    api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
+class Youtube:
+    """Базовый класс для работы с видео, плейлистами и каналами"""
 
-    def __init__(self, channel_id: str) -> None:
-        """
-        Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API.
-        """
-        self.__channel_id = channel_id
-        self.channel = self.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-        self.title = self.channel['items'][0]['snippet']['title']
-        self.description = self.channel['items'][0]['snippet']['description']
-        self.url = f"https://www.youtube.com/channel/{channel_id}"
-        self.video_count = self.channel['items'][0]['statistics']['videoCount']
-        self.view_count = self.channel['items'][0]['statistics']['viewCount']
-        self.subscriber_count = int(self.channel['items'][0]['statistics']['subscriberCount'])
+    __API_KEY: str = os.getenv('YT_API_KEY')
+    __youtube = build('youtube', 'v3', developerKey=__API_KEY)
 
-    def print_info(self) -> None:
+    @classmethod
+    def get_playlist_info(cls, playlist_id: str) -> dict:
         """
-        Выводит в консоль информацию о канале.
+        Метод для получения информации о плейлисте Youtube по его id
         """
-        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
 
-    @property
-    def channel_id(self):
-        return self.channel_id
+        playlist_response = cls.__youtube.playlists().list(id=playlist_id,
+                                                           part='contentDetails,snippet',
+                                                           maxResults=50,
+                                                           ).execute()
+        return playlist_response
+
+    @classmethod
+    def get_video_info(cls, video_id: str) -> dict:
+        """Метод для получения информации о видео из Youtube по его id"""
+
+        video_response = cls.__youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                     id=video_id
+                                                     ).execute()
+        return video_response
+
+    @classmethod
+    def get_channel_info(cls, channel_id: str) -> dict:
+        """Метод для получения информации о канале Youtube по его id"""
+
+        channel_response = cls.__youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+
+        return channel_response
+
+    @classmethod
+    def get_videos_from_playlist(cls, playlist_id: str) -> dict:
+        """Получение информации о всех видео из плейлиста"""
+        playlist_videos = cls.__youtube.playlistItems().list(playlistId=playlist_id,
+                                                             part='contentDetails',
+                                                             maxResults=50,
+                                                             ).execute()
+        return playlist_videos
 
     @classmethod
     def get_service(cls):
-        """
-        Возвращает объект для работы с YouTube API.
-        """
-        return cls.youtube
+        """Возвращает объект для работы с API вне класса"""
+        return cls.__youtube
 
-    def to_json(self, filename: str):
-        """
-        Сохраняет в файл значения атрибутов экземпляра Channel.
-        """
-        with open(filename, 'w', encoding='utf-8') as file:
-            json.dump(self.__dict__, file, ensure_ascii=False, indent=4)
 
-    def __str__(self):
-        return f"{self.title}, ({self.url})"
+class Channel(Youtube):
+    """Класс для ютуб-канала"""
 
-    def __add__(self, redactsiya) -> int:
-        if isinstance(redactsiya, Channel):
-            return self.subscriber_count + redactsiya.subscriber_count
+    def __init__(self, channel_id: str) -> None:
+        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+
+        self.__channel_id = channel_id
+        self.channel = self.get_channel_info(channel_id)
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
+        self.subscribers_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.views_count = self.channel['items'][0]['statistics']['viewCount']
+
+    @property
+    def channel_id(self):
+        """Свойство для обращения к приватному атрибуту __channel_id"""
+        return self.__channel_id
+
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+        print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    def to_json(self, file_name: str) -> None:
+        """Запись информации о канале в file_name.json"""
+        with open(file_name, 'w') as json_file:
+            json.dump(self.channel, json_file, ensure_ascii=False)
+
+    def __str__(self) -> str:
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other) -> int:
+        """Сложение классов по количеству подписчиков"""
+        return int(self.subscribers_count) + int(other.subscribers_count)
+
+    def __sub__(self, other) -> int:
+        """Разность классов по количеству подписчиков"""
+        if isinstance(other, Channel):
+            return int(self.subscribers_count) - int(other.subscribers_count)
         else:
             raise TypeError("ERROR")
 
-    def __sub__(self, redactsiya) -> int:
-        if isinstance(redactsiya, Channel):
-            return self.subscriber_count - redactsiya.subscriber_count
+    def __ge__(self, other) -> bool:
+        """Сравнение классов по количеству подписчиков"""
+        if isinstance(other, Channel):
+            return int(self.subscribers_count) >= int(other.subscribers_count)
         else:
             raise TypeError("ERROR")
 
-    def __gt__(self, redactsiya):  # >
-        if isinstance(redactsiya, Channel):
-            return self.subscriber_count > redactsiya.subscriber_count
+    def __gt__(self, other) -> bool:
+        """Сравнение классов по количеству подписчиков"""
+        if isinstance(other, Channel):
+            return int(self.subscribers_count) > int(other.subscribers_count)
         else:
             raise TypeError("ERROR")
 
-    def __ge__(self, redactsiya) -> int:  # >=
-        if isinstance(redactsiya, Channel):
-            return self.subscriber_count >= redactsiya.subscriber_count
+    def __lt__(self, other) -> bool:
+        """Сравнение классов по количеству подписчиков"""
+        if isinstance(other, Channel):
+            return int(self.subscribers_count) < int(other.subscribers_count)
         else:
             raise TypeError("ERROR")
 
-    def __lt__(self, redactsiya):  # <
-        if isinstance(redactsiya, Channel):
-            return self.subscriber_count < redactsiya.subscriber_count
+    def __le__(self, other) -> bool:
+        """Сравнение классов по количеству подписчиков"""
+        if isinstance(other, Channel):
+            return int(self.subscribers_count) <= int(other.subscribers_count)
         else:
             raise TypeError("ERROR")
 
-    def __le__(self, redactsiya):  # <=
-        if isinstance(redactsiya, Channel):
-            return self.subscriber_count <= redactsiya.subscriber_count
-        else:
-            raise TypeError("ERROR")
-
-    def __eq__(self, redactsiya):  # ==
-        if isinstance(redactsiya, Channel):
-            return self.subscriber_count == redactsiya.subscriber_count
+    def __eq__(self, other) -> bool:
+        """Сравнение классов по количеству подписчиков"""
+        if isinstance(other, Channel):
+            return int(self.subscribers_count) == int(other.subscribers_count)
         else:
             raise TypeError("ERROR")
--- src/video.py
+++ src/video.py
@@ -1,74 +1,34 @@
-from src.channel import Channel
+from src import channel
 
 
-class Video:
-    """
-    Инициализация класса
-    """
+class Video(channel.Youtube):
 
     def __init__(self, video_id):
-        self.__video_id = video_id
-        self.__video = self.video
-        self.__title = self.title
-        self.__url = self.url
-        self.__view_count = self.view_count
-        self.__likes_count = self.likes_count
+        """
+        Инициализация атрибутов класса
+        """
 
-    @property
-    def video_id(self):
-        """
-        Возвращает id видео
-        """
-        return self.__video_id
+        # Получение информации о видео
+        self.video_response = self.get_video_info(video_id)
 
-    @property
-    def video(self):
-        """
-        Запрос к youtube по определенному id
-        """
-        return Channel.get_service().videos().list(
-            part='snippet,statistics,contentDetails,topicDetails',
-            id=self.__video_id
-        ).execute()
-
-    @property
-    def title(self):
-        """
-        Возвращает данные видео
-        """
-        return self.video['items'][0]['snippet']['title']
-
-    @property
-    def url(self):
-        """
-        Возвращает ссылку на видео
-        """
-        return f'https://www.youtube.com/watch?v={self.__video_id}'
-
-    @property
-    def view_count(self):
-        """
-        Возвращает количество просмотров
-        """
-        return self.video['items'][0]['statistics']['viewCount']
-
-    @property
-    def likes_count(self):
-        """
-        Возвращает количество лайков
-        """
-        return self.video['items'][0]['statistics']['likeCount']
+        # Заполнение атрибутов соответствующими данными
+        self.video_id = video_id
+        self.video_title = self.video_response['items'][0]['snippet']['title']
+        self.video_url = f'https://www.youtube.com/video/{self.video_id}'
+        self.views_count = self.video_response['items'][0]['statistics']['viewCount']
+        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+        self.duration = self.video_response['items'][0]['contentDetails']['duration']
 
     def __str__(self):
-        return self.title
+        return self.video_title
 
 
 class PLVideo(Video):
-    """
-    Класс для видео `PLVideo`, который инициализирует 'id видео' и 'id плейлиста из класса Video'
-    """
 
     def __init__(self, video_id, playlist_id):
-        self.__video_id = video_id
-        self.__playlist_id = playlist_id
+        """
+        Инициализация атрибутов класса
+        """
+
         super().__init__(video_id)
+        self.playlist_id = playlist_id
