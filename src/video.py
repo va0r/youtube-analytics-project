0850c0bee07cb3b864b64fde0875ac4b6572a991
viktorlov <orlovva@ymail.com>
2023-05-24 13:25:33 +0300

    (l) code review <<homework-6>>

--- src/video.py
+++ src/video.py
@@ -1,3 +1,5 @@
+from googleapiclient.errors import HttpError
+
 from src import channel
 
 
@@ -9,15 +11,27 @@ class Video(channel.Youtube):
         """
 
         # Получение информации о видео
-        self.video_response = self.get_video_info(video_id)
+        try:
+            # Пробуем получить информацию о видео по переданному ID
+            self.video_response = self.get_video_info(video_id)
 
-        # Заполнение атрибутов соответствующими данными
-        self.video_id = video_id
-        self.video_title = self.video_response['items'][0]['snippet']['title']
-        self.video_url = f'https://www.youtube.com/video/{self.video_id}'
-        self.views_count = self.video_response['items'][0]['statistics']['viewCount']
-        self.like_count = self.video_response['items'][0]['statistics']['likeCount']
-        self.duration = self.video_response['items'][0]['contentDetails']['duration']
+            # Заполнение атрибутов соответствующими данными
+            self.video_id = video_id
+            self.video_title = self.video_response['items'][0]['snippet']['title']
+            self.video_url = f'https://www.youtube.com/video/{self.video_id}'
+            self.views_count = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count = self.video_response['items'][0]['statistics']['likeCount']
+            self.duration = self.video_response['items'][0]['contentDetails']['duration']
+        except (HttpError, IndexError):
+            # В случае некорректного ID исключение HttpError не вызывается,
+            # по ключу Items будет пустой список и при обращении к нему получим ошибку IndexError
+
+            self.video_id = video_id
+            self.title = None
+            self.video_url = None
+            self.views_count = None
+            self.like_count = None
+            self.duration = None
 
     def __str__(self):
         return self.video_title
